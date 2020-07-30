a8f63523642cef832203ff119cf01e29ca32e211
accolombini <accolombini@gmail.com>
2020-07-30 14:58:45 -0300

    Uso do help e dir para suporte à programação

---
+++ lab2_dir_help.py
@@ -0,0 +1,12 @@
+"""
+São utulitários Python para auxiliar na programação.
+dir => Apresenta todos os atributos/propriedades e funçõs/métodos disponíveis para determinado tipo de dado ou variável.
+dir(tipo de dado/variável)
+help => Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dados ou variável.
+help(tipo de dados/variável.propriedade) -> retorna documentação de propriedade
+
+print(dir('Teste'))
+
+"""
+
+print(dir('Teste'))

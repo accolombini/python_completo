025a6a68454c1b238a37683682f28bab6a16e6e6
accolombini <accolombini@gmail.com>
2020-07-30 20:11:38 -0300

    Trabalhando com tipo numérico

---
+++ lab4_tipo_numerico.py
@@ -0,0 +1,18 @@
+"""
+    summary[Tipos de variáveis em Python - variáveis numéricas]
+"""
+'''
+Python permite que use undeline para separ milhar facilitando sua visualização => 1_000_000_000 ->> será lido como 1000000000 pelo Python
+Posso visualizar o tipo de uma variável/valor usando o comando type(valor)
+Para saber odas as operações que poderá realizar use => dir(valor)
+Algumas operações interessantes com números são descritas a seguir:
+1- 5 / 2 = 2.5
+2- 5 // 2 = 2
+3- 4 % 2 = 0
+4- 5 % 2 = 1
+5- 3 ** 3 = 27
+
+'''
+
+num = 1_000_000_000
+print(f"O valor desse número será expresso como {num}")

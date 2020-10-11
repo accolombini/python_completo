935102173a1112273b09734392dca08d76e9c749
accolombini <accolombini@gmail.com>
2020-10-11 20:39:08 -0300

    Conhecendo o SintaxWarning

---
+++ lab117_sintaxes_perigosas.py
@@ -0,0 +1,9 @@
+"""
+Novidades da versão 3.8 =>  Sintaxes Perigosas ->> sintaxes warnnings SyntaxWarning ->> a sintaxe
+                            não está errada mas poderá resultar em problemas. Exemplos:
+                            - == => é usado para checar valor
+                            - is => é usado para checar se os objetos são os mesmos
+                            - Para aproveitar esse recurso o programador deverá ficar atento às mensagens
+                            de erro, principalmente nas SyntaxWarning que estará tentando sugerir algo que
+                            poderá ser pertinente ao seu código
+"""
